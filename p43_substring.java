962bcf107d9d3342cee93cee379067dd91f3e543
ManjuMuralidharan <manju1695@gmail.com>
2016-08-29 21:46:31 +0530

    Add files via upload

---
+++ p43_substring.java
@@ -0,0 +1,21 @@
+package p43_substring;
+
+public class p43_substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="abate";
+		String s2="bat";
+		
+		if(s1.contains(s2))
+		{
+			System.out.println("Substring");
+		}
+		else
+		{
+			System.out.println("Not a Substring");
+		}
+
+	}
+
+}
